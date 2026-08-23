7be58e67ede5efd16f636d4ce3e740f15fb60e9a
Sadiya-Mehreen <sadiyamehreenaiml@gmail.com>
2026-08-23 15:17:22 +0530

    Import crewai.cache and set cache breakpoint

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+import crewai.llms.cache as _crewai_cache
 
+_crewai_cache.mark_cache_breakpoint = lambda msg: msg
 from api.routes import router
 
 load_dotenv()
